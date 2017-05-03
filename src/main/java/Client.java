59ac35a516fca3e4577256be85fef4af522642d5
vika <zc.vika@gmail.com>
2017-05-03 18:43:09 +0300

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by vika on 03.05.17.
+ */
+public class Client {
+    public static void forClient() {
+        Socket clientSocket = null;
+        try {
+            clientSocket = new Socket("localhost", 4444);
+            //получаем OutputStream
+            OutputStream outputStream = clientSocket.getOutputStream();
+            PrintWriter out = new PrintWriter(outputStream, true);
+            out.println("Kiss my shiny metal ass!");
+            out.flush();
+
+            //читаем ответ
+            InputStream inputStream = clientSocket.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            String answer = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+/**
+ * Created by vika on 03.05.17.
+ */
+public class Program {
+    public static void main(String[] args) {
+        Client.forClient();
+
+        Server.forServer();
+    }
+}
