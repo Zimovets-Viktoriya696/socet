4ab742ec52ae2181f2400343c0baa96287fbb9c0
vika <zc.vika@gmail.com>
2017-07-19 17:44:20 +0300

    initial

--- src/main/java/Link.java
+++ src/main/java/Link.java
@@ -20,26 +20,20 @@ public class Link {
         Socket clientSocket = null;
         try {
             clientSocket = new Socket("localhost", 4444);
-            //получаем OutputStream
-            OutputStream outputStream = clientSocket.getOutputStream();
-            PrintWriter out = new PrintWriter(outputStream, true);
-            out.println("Kiss my shiny metal ass!");
-            out.flush();
-
-            //читаем ответ
-            InputStream inputStream = clientSocket.getInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
-            String answer = in.readLine();
-            System.out.println(answer);
+            clientSocket.getInputStream();
+            clientSocket.getOutputStream();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void sendMessage (Message message){
+    public String sendMessage (Message message){
         Gson gson = new Gson();
         String json = gson.toJson(message);
+        return json;
     }
+
+
 }
 
 
